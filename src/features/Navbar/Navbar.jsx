ac619cd19d93bc104a7134808392c5921fb38a8f
Shreyas0812 <shreyasraorane8@gmail.com>
2024-08-04 19:39:32 +0530

    making navbar responsive

--- src/features/Navbar/Navbar.jsx
+++ src/features/Navbar/Navbar.jsx
@@ -17,13 +17,13 @@ function Navbar() {
   return (
     <>
       <nav className="bg-gray-100">
-        <div className="mx-auto max-w-full px-8 sm:px-4 lg:px-8">
+        <div className="mx-auto max-w-full px-8 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex flex-1 items-center justify-center sm:justify-start">
               <div className="flex flex-shrink-0 items-center">
-                <img className="w-40" src={LOGO} alt="PGCC LOGO"></img>
+                <img className="w-32 sm:w-40" src={LOGO} alt="PGCC LOGO" />
               </div>
-              <div className="hidden sm:ml-6 sm:block">
+              <div className="hidden sm:ml-6 sm:flex">
                 <div className="flex space-x-4">
                   {tabList.map((tabDetails) => {
                     return (
@@ -45,7 +45,7 @@ function Navbar() {
             </div>
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
               <div className="relative ml-3">
-                <div className="hidden sm:ml-6 sm:block">
+                <div className="sm:block">
                   <div className="flex space-x-4">
                     <Link
                       to={`/${CONTACTUS}`}
