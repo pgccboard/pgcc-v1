6a10fa925f68298bed63ccbd61a0527f8b4fd86d
Shreyas0812 <shreyasraorane8@gmail.com>
2024-08-06 00:04:51 +0530

    Landing page made responsive

--- src/features/Home/Landing/Landing.jsx
+++ src/features/Home/Landing/Landing.jsx
@@ -4,23 +4,28 @@ import BENFRANK from "../../../assets/benfrank.png";
 import { ABOUT, CONTACTUS } from "../../../pgccConstants";
 
 function Landing() {
+  // 'sm': '640px',
+  //     'md': '768px',
+  //     'lg': '1024px',
+  //     'xl': '1280px',
+  //     '2xl': '1536px',
   return (
     <>
-      <div className="flex flex-col md:flex-row items-center md:justify-center p-8 md:p-16">
-        <div className="text-center md:text-left md:mr-8 mb-8 md:mb-0">
-          <h1 className="text-5xl md:text-6xl text-gray-800 font-bold mb-4">
+      <div className="flex flex-col lg:flex-row items-center lg:justify-center p-8 md:p-16">
+        <div className="text-center lg:text-left lg:mr-8 mb-8 lg:mb-0">
+          <h1 className="text-5xl lg:text-4xl text-gray-800 font-bold mb-4">
             Penn Graduate
           </h1>
-          <h1 className="text-5xl md:text-6xl text-gray-800 font-bold mb-4">
+          <h1 className="text-5xl lg:text-4xl text-gray-800 font-bold mb-4">
             Consulting Club
           </h1>
-          <p className="text-lg md:text-2xl text-gray-600">
+          <p className="text-lg md:text-xl text-gray-600">
             Premiere student-run consulting club at the
           </p>
-          <p className="text-lg md:text-2xl text-gray-600 mb-6">
+          <p className="text-lg md:text-xl text-gray-600 mb-6">
             University of Pennsylvania, 500+ members strong.
           </p>
-          <div className="flex justify-center md:justify-start space-x-4">
+          <div className="flex justify-center lg:justify-start space-x-4">
             <Link to={`/${ABOUT}`}>
               <button className="bg-tamarillo text-white py-2 px-4 rounded-lg shadow hover:bg-burgundy">
                 Learn More
@@ -34,9 +39,9 @@ function Landing() {
           </div>
         </div>
 
-        <div className="w-full md:w-auto">
+        <div className="w-full lg:w-auto">
           <img
-            className="w-full h-auto max-w-md mx-auto md:mx-0 rounded-lg shadow"
+            className="w-full h-auto max-w-md mx-auto lg:mx-0 rounded-lg shadow"
             src={BENFRANK}
             alt="PGCC LOGO"
           ></img>
