f79d1b8730a1c3d764bf51cd60cf71eef4fd72b5
Shreyas0812 <shreyasraorane8@gmail.com>
2024-08-17 00:52:42 +0530

    edited overview to include button and image children if available

--- src/features/Overview/Overview.jsx
+++ src/features/Overview/Overview.jsx
@@ -1,11 +1,14 @@
-function Overview({ children }) {
+function Overview({ children, content }) {
   return (
     <>
       <div className="flex flex-col items-center justify-center bg-gradient-to-br from-tamarillo/20 to-cello/50 h-56">
         <div className="flex flex-col lg:flex-row items-center lg:justify-center p-8 md:p-12 max-w-screen-xl">
           <div className="text-center lg:text-left lg:mr-8 mb-8 lg:mb-0">
-            <p className="text-lg md:text-xl text-gray-900">{children}</p>
+            <p className="text-lg md:text-xl text-gray-900">{content}</p>
+            <br />
+            {children && children[0]}
           </div>
+          {children && children[1]}
         </div>
       </div>
     </>
