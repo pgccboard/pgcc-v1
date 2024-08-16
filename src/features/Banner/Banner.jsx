6019e7df0ee2d09317e7c18f90d2aaac04ef8c66
Shreyas0812 <shreyasraorane8@gmail.com>
2024-08-16 21:21:25 +0530

    height defined and centered items vertically

--- src/features/Banner/Banner.jsx
+++ src/features/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 function Banner({ title, subTitle }) {
   return (
     <>
-      <div className="flex flex-col items-center bg-gradient-to-bl from-cello/50 to-tamarillo/20">
+      <div className="flex flex-col items-center justify-center bg-gradient-to-bl from-cello/50 to-tamarillo/20 h-64">
         <div className="flex flex-col lg:flex-row items-center lg:justify-center p-8 md:p-12 max-w-screen-xl">
           <div className="text-center lg:text-left lg:mr-8 mb-8 lg:mb-0">
             <h1 className="text-5xl lg:text-4xl text-cello font-bold mb-4 text-center">
