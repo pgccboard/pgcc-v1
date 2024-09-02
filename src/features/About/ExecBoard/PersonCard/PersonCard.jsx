03a4d431b622cd38164acedf1deb1c69ffa3f644
Shreyas0812 <shreyasraorane8@gmail.com>
2024-09-02 19:38:45 -0400

    PersonCards added

---
+++ src/features/About/ExecBoard/PersonCard/PersonCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import LinkedIn from "../../../../assets/icons/linkedin.png";
+import Mail from "../../../../assets/icons/email.png";
+import { Link } from "react-router-dom";
+
+const PersonCard = ({ person }) => (
+  <div className="bg-white border rounded-lg overflow-hidden my-2">
+    <img className="w-full object-cover" src={person.image} alt={person.name} />
+    <div className="p-5">
+      <div>
+        <p className="font-bold text-xl">{person.name}</p>
+        <p className="text-sm italic text-gray-600">{person.title}</p>
+      </div>
+      <div className="flex flex-wrap justify-between mt-5">
+        <div className="flex">
+          <Link to={person.linkedin} target="_blank">
+            <img src={LinkedIn} className="w-8 h-8 mr-2" alt="linkedin" />
+          </Link>
+
+          {person.email && (
+            <Link to={`mailto:${person.email}`}>
+              <img src={Mail} className="w-8 h-8" alt="mail" />
+            </Link>
+          )}
+        </div>
+        <button type="button" className="link-btn self-end">
+          Read bio →
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+export default PersonCard;
